133c332eb40a3784ccbb0eee379cc963c64eece5
Sergiu <sergiu.uhu@gmail.com>
2023-12-31 19:18:18 +0200

    link external

--- src/modules.js
+++ src/modules.js
@@ -55,6 +55,10 @@ const Modules = (p) => {
             ))}
           </select>
         </div>
+
+        <a className="abtn" style={styles.shop} href="https://shop.shadowenglish.com/" target="_blank">
+        🎁 Shop 
+        </a>
       </div>
 
       <div style={styles.modules}>
@@ -149,6 +153,17 @@ const emojis = [
 ];
 
 const styles = {
+  shop: {
+    position: "absolute",
+    top: "5px",
+    right: 0,
+    background: "rgba(255, 255, 255, 0.1)",
+    color: "#fff",
+    border: "none",
+    padding: "10px 15px",
+    borderRadius: "3px",
+    fontWeight: 600
+  },
   preferences: {
     padding: "10px 0 0 0",
     display: "flex",
